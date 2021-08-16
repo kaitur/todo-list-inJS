e4e39183a3b348fbf3fac88f39acbdbe968297fb
Artur Kaidanskyi <artur.kaidanskyi@gmail.com>
2021-08-16 18:18:39 +0300

    basic functionality ToDo list

---
+++ script/scripts.js
@@ -0,0 +1,56 @@
+class Tasks {
+    constructor(title_, description_, dueDate_, done_) {
+        // this.taskId = taskId_;
+        this.title = title_;
+        this.description = description_;
+        this.dueDate = dueDate_;
+        this.done = done_;
+    }
+}
+
+let taskList = [
+    new Tasks('Научиться спать не более 5 часов в сутки', 'Здесь какое-то описание', '2024.05.10', true),
+    new Tasks('Начать ходить к психотерапевту', 'Здесь какое-то описание', '2047.11.05', false),
+    new Tasks('Увидеть вживую Илона Маска', 'Здесь какое-то описание', '', false),
+    new Tasks('Получить первый госзакз', '', '', false),
+    new Tasks('Сменить бизнес модель', '', '', true),
+    new Tasks('Попасть в список Forbes', '', '1993.03.15', true),
+    new Tasks('Поужинать в компании Джеффа Безоса', 'Здесь какое-то описание', '1981.07.05', true)
+];
+
+
+
+const listItem = document.getElementById('taskList');
+function addTask(task) {
+    // listItem.innerHTML += `<div>${task.title}</div>`
+    listItem.innerHTML +=
+        '<div class="tasks-container">' +
+        '<div class="task">' +
+        `<h3 class="task__title${isComplete(task.done)}">
+            ${task.title}
+        </h3>` +
+        `<p class="task__text">
+            ${task.description}
+        </p>` +
+        `<p class="task__text">
+            ${task.dueDate}
+        </p>` +
+        '<button class="task__toggle">' +
+        '<i class="fas fa-chevron-down"></i>' +
+        '<i class="fas fa-times"></i>' +
+        '</button>'
+    '</div>' +
+        '</div>';
+}
+
+function isComplete(done) {
+    return done ? '__complete' : '';
+}
+taskList.forEach(addTask);
+
+const toggles = document.querySelectorAll('.task__toggle')
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
